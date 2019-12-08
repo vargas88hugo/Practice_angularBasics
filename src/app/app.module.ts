a38c4a83b1866186ef58ba814c30ce04d13e7a8e
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-08 17:29:26 -0500

    [Feature] added services and component of client in the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectiveComponent } from './directive/directive.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ClientService } from './clients/client.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DirectiveComponent
+    DirectiveComponent,
+    ClientsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ClientService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
